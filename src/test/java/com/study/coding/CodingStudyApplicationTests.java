bd4d21d34748d02af3dd3c1ac35b60726496994c
soohyunnn <soohyunnn@gmail.com>
2021-12-18 09:48:57 +0900

    first commit

---
+++ src/test/java/com/study/coding/CodingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
